6e54433b3c845f2eae210c74ab064a3798cc48e7
Ram Adusumilli <ramadusumilli@Rams-MacBook-Pro.local>
2021-02-27 20:53:32 -0600

    1.Added routes for main and dashboard. 2. Added basic layouts for main and dashboard 3. Set the handlebars for routers using express handlebars

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db")
 const morgan = require("morgan");
+const exphbs = require("express-handlebars");
 // Load config
 dotenv.config({path: "./config/config.env"})
 
@@ -14,6 +15,14 @@ const app = express()
 if(process.env.NODE_ENV === "development") {
     app.use(morgan("dev"))
 }
+
+// Handlebars - Setting up the view engine and default layout
+app.engine(".hbs", exphbs({defaultLayout: "main", extname: ".hbs"}))
+app.set("view engine", ".hbs")
+
+// Routes
+app.use("/", require("./routes/index"))
+
 
 const PORT = process.env.PORT || 3000 
 
